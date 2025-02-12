8f4cac06a21c5824e913574e92f19e65414febfc
yogesh sharma <yogeshsharma2003kh@gmail.com>
2025-02-12 11:49:38 +0530

    second

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config(); 
 mongoose.connect(process.env.MONGODB_URI, { 
- 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(() => console.log("Connected to MongoDB Atlas"))
   .catch(err => console.error("MongoDB connection error:", err));
 const userSchema=mongoose.Schema({
