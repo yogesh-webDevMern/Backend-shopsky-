402266f2487f443e060a1d9b115edf6ace880052
yogesh sharma <yogeshsharma2003kh@gmail.com>
2025-02-12 12:52:06 +0530

    OKKK

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
 require("dotenv").config(); 
+const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI, { 
 }).then(() => console.log("Connected to MongoDB Atlas"))
   .catch(err => console.error("MongoDB connection error:", err));
