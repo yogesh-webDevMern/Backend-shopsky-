5d2b5457787f715f3ebc18b6dc3735796c79bb45
yogesh sharma <yogeshsharma2003kh@gmail.com>
2025-02-12 11:20:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const userModel = require("./models/user");
+const path = require("path");
+app.set("view engine","ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"public")));
+app.get("/",(req,res)=>
+{
+ res.render("index")
+})
+app.post("/create",async(req,res)=>
+{
+    let {name,email,image,contact,gender,age} = req.body;
+        let createdUser =await userModel.create(
+        {
+            name,
+            email,
+            image,
+            contact,
+            gender,
+            age
+        }
+    )
+    res.redirect("/show");
+})
+app.get("/show",async(req,res)=>
+{
+   let users =await userModel.find();
+res.render("show",{users});
+})
+app.get("/delete/:id",async(req,res)=>
+{
+ let deleteuser = await userModel.findOneAndDelete({_id:req.params.id});
+res.redirect("/show");
+})
+const PORT = process.env.PORT || 3005;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config(); 
+mongoose.connect(process.env.MONGODB_URI, { 
+ 
+}).then(() => console.log("Connected to MongoDB Atlas"))
+  .catch(err => console.error("MongoDB connection error:", err));
+const userSchema=mongoose.Schema({
+    name:String,
+    email:String,
+    image:String,
+    contact:Number,
+    gender:String,
+    age:Number
+})
+module.exports=mongoose.model("user",userSchema);
